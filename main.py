54c3435a74cb5145e219fa01c1f51bb1a358776f
Sean Dutton-Jones <sddutton@sfu.ca>
2021-01-06 16:05:13 -0800

    added intermediate image displays

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 import cv2 as cv
 from matplotlib import pyplot as plot
 
-img = cv.imread("plant@2x.jpg", 0)
+# helper function to easily display our images
+def img_show(title, image):
+    plot.title(title)
+    plot.xticks([])
+    plot.yticks([])
+    plot.imshow(image, cmap="gray")
+    plot.show()
 
+# read in our original image as grayscale
+img = cv.imread("original@2x.jpg", cv.IMREAD_GRAYSCALE)
+
+# show grayscale image using our helper function
+img_show("Grayscale Image", img)
+cv.imwrite("grayscale.jpg", img)
+
+# blurring the image with a 5x5, sigma = 1 Guassian kernel
+img_blur = cv.GaussianBlur(img, (5, 5), 1)
+
+img_show("Gaussian Blur", img_blur)
+cv.imwrite("gaussian_blur.jpg", img_blur)
+
+# applying both horizontal and vertical 3x3 Sobel kernels
+img_sobelx = cv.Sobel(img_blur, cv.CV_64F, 1, 0, ksize=3)
+img_sobely = cv.Sobel(img_blur, cv.CV_64F, 0, 1, ksize=3)
+
+img_show("Horizontal(x) Sobel", cv.convertScaleAbs(img_sobelx))
+img_show("Vertical(y) Sobel", cv.convertScaleAbs(img_sobely))
+cv.imwrite("sobel_x.jpg", img_sobelx)
+cv.imwrite("sobel_y.jpg", img_sobely)
+
+# finally, generate canny edges
 # extreme examples: high threshold [900, 1000]; low threshold [1, 10]
-edges = cv.Canny(img, 50, 100)
+img_edges = cv.Canny(img, 50, 100)
 
-# sets the plot's title
-plot.title("Canny Edges")
-
-# removes the numbers from the x and y axis
-plot.xticks([]), plot.yticks([])
-
-# shows the final plot
-plot.imshow(edges, cmap="gray")
-plot.show()
+img_show("Canny Edges", img_edges)
+cv.imwrite("canny_edges.jpg", img_edges)
